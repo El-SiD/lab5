ebec7c78647177550bf597c92b6415c6ec4bdd67
Mikhail Tsvetkov <mikhail.tsvetkov.94@gmail.com>
2012-04-29 01:00:49 +0400

    Done but need to optimise (n^2 -> n log n)

--- lab5.java
+++ lab5.java
@@ -1,103 +1,110 @@
 import java.io.*;
 import java.util.*;
-
 import javax.swing.JOptionPane;
 
 	public class lab5 
 	{
-		public static void main(String[] args)
+		public static void main(String[] args) throws IOException
 		{
-			try
+			File Text = new File("File.txt");
+			if (Text.exists()) Text.delete();
+			Text.createNewFile();
+			File Output = new File("Output.txt");
+			if (Output.exists()) Output.delete();
+			Output.createNewFile();
+			FileWriter FileOut = new FileWriter(Text);
+			BufferedWriter Writer = new BufferedWriter(FileOut);
+			String Tmp = "";
+			long NumberOfStrings = 20, Length = 5; // 4GB = 536870912 UTF-8 Chars (example 33554432 strings by 16 chars)
+			for (int i = 0; i < NumberOfStrings; i++)
 			{
-				Merge("1.txt", "2.txt", "3.txt");
-				RandomAccessFile File = new RandomAccessFile(new File("File.txt"), "rw");
-				String Tmp = "";
-				long NumberOfStrings = 20, Length = 5; // 4GB = 536870912 UTF-8 Chars (example 33554432 strings by 16 chars)
-				for (int i = 0; i < NumberOfStrings; i++)
-				{
-					for (int j = 0; j < Length; j++)
-						Tmp += (char)(26 * Math.random() + 64);
-					Tmp += "\n";
-					File.writeUTF(Tmp);
-					Tmp = "";
-				}
-				JOptionPane.showMessageDialog(null, "Заполнение завершено.");
-				long Time = System.currentTimeMillis();
-				int BufferSize = 550; // in strings
-				List<String> Part = new LinkedList<String>();
-				Tmp = File.readLine();
-				int PartNumber = 0;
-				do
-				{
-					Part.add(Tmp);
-					if (Part.size() >= BufferSize) 
-					{	
-						Collections.sort(Part);
-						Part.clear();
-						PartNumber++;
-					}
-					Tmp = File.readLine();
-				}
-				while (Tmp != null);
-				for (int i = 0; i < PartNumber - 1; i++)
-				{
-					Merge(i + ".txt", i + 1 + ".txt", PartNumber + i + ".txt");
-				}
-				Time = System.currentTimeMillis() - Time;
-				JOptionPane.showMessageDialog(null, "Сортировка завершена. Время: " + Time);
-				
+				for (int j = 0; j < Length; j++)
+					Tmp += (char)(26 * Math.random() + 64);
+				Tmp += "\n";
+				Writer.write(Tmp);
+				Tmp = "";
 			}
-			catch(Exception e)
+			Writer.close();
+			JOptionPane.showMessageDialog(null, "Заполнение завершено.");
+			long Time = System.currentTimeMillis();
+			int BufferSize = 5; // in strings (optimal 500 - 600)
+			List<String> Part = new LinkedList<String>();
+			FileReader FileIn = new FileReader(Text);
+			BufferedReader Reader = new BufferedReader(FileIn);
+			Tmp = Reader.readLine();
+			do
 			{
-			/*	JOptionPane.showMessageDialog(null, e.toString());
-				e.printStackTrace(); 
-			*/
+				Part.add(Tmp);
+				if (Part.size() >= BufferSize) 
+				{	
+					Collections.sort(Part);
+					Merge(new File(Output.getName()), Part, new File("Output.tmp"));
+					Part.clear();
+				}
+				Tmp = Reader.readLine();
 			}
+			while (Tmp != null);
+			Reader.close();
+			//Text.delete();
+			Time = System.currentTimeMillis() - Time;
+			JOptionPane.showMessageDialog(null, "Сортировка завершена. Время: " + Time);
+			if (IsSorted(Output)) JOptionPane.showMessageDialog(null, "Файл отсортирован верно"); else JOptionPane.showMessageDialog(null, "Файл отсортирован НЕ верно");
+			//Output.delete();
 		}
-		static void Merge(String input1, String input2, String output) throws IOException
+		static void Merge(File input, List<String> Part, File output) throws IOException
 		{
-			FileReader file_in1 = new FileReader(input1);
-			BufferedReader reader1 = new BufferedReader(file_in1);
-			FileReader file_in2 = new FileReader(input2);
-			BufferedReader reader2 = new BufferedReader(file_in2);
+			output.createNewFile();
+			FileReader file_in = new FileReader(input);
+			BufferedReader reader = new BufferedReader(file_in);
 			FileWriter file_out = new FileWriter(output);
 			BufferedWriter writer = new BufferedWriter(file_out);
-			String m1, m2;
-			List<String> Part = new LinkedList<String>();
-			m1 = reader1.readLine();
-			m2 = reader2.readLine();
-			do
+			String tmp;
+			tmp = reader.readLine();
+			while (tmp != null)
 			{
-				Part.add(m2);
-				m2 = reader2.readLine();
-			}
-			while (m2 != null);
-			do
-			{
-				if (Part.size() == 0 || m1.compareTo(Part.get(0)) <= 0)
+				if (Part.size() == 0 || tmp.compareTo(Part.get(0)) <= 0)
 				{
-					writer.write(m1 + "\n");
-					m1 = reader1.readLine();
+					writer.write(tmp + "\n");
+					tmp = reader.readLine();
 				}
 				else
 				{
 					writer.write(Part.get(0) + "\n");
 					Part.remove(0);
-					writer.write(m1 + "\n");
-					m1 = reader1.readLine();
 				}
 			}
-			while (m1 != null);
-			for (;;)
-				if (Part.size() != 0)
-				{	
-					writer.write(Part.get(0) + "\n");
-					Part.remove(0);
-				}
-				else break;
+			while (Part.size() != 0)
+			{	
+				writer.write(Part.get(0) + "\n");
+				Part.remove(0);
+			}
 			Part.clear();
-			reader1.close();
-			reader2.close();
+			reader.close();
 			writer.close();
+			input.delete();
+			output.renameTo(input);
+		}
+		static boolean IsSorted(File file) throws IOException
+		{
+			String tmp1, tmp2;
+			FileReader file_in = new FileReader(file);
+			BufferedReader reader = new BufferedReader(file_in);
+			tmp1 = reader.readLine();
+			tmp2 = reader.readLine();
+			while (tmp2 != null)
+			{
+				if (tmp1.compareTo(tmp2) > 0)
+				{
+					reader.close();
+					return false;
+				}
+				else
+				{
+					tmp1 = reader.readLine();
+					tmp2 = reader.readLine();
+				}
+			}
+			reader.close();
+			return true;
 		}
 	}
